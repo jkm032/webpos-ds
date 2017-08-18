5519176c96f9b12872bc0db1a82df42aaac4aef6
zhangtao <2850289679@qq.com>
2017-08-18 18:32:01 +0800

    sidebar

--- webpos-DS/src/components/navs/sidebar.vue
+++ webpos-DS/src/components/navs/sidebar.vue
@@ -1,10 +1,24 @@
 <template>
-<div class="sidebar">侧边栏导航</div>
+<div class="sidebar">侧边栏导航
+    <ul>
+        <!-- <li><router-link :to="/orders">订单</router-link></li>
+        <li><router-link :to="/transform">交班</router-link></li>
+        <li><router-link :to="/sales-rtn">退货</router-link></li> -->
+    </ul>
+</div>
 </template>
 <script>
-export default {
-  
-}
+//     import Nav2 from './navs/nav2'
+// 	import CartLeft from './carts/cart-left'
+// 	import CartRight from './carts/cart-right'
+// export default {
+//   components:{
+// 		'ctntop': Nav1,
+// 		'ctnleft': CartLeft,
+// 		'ctnright': CartRight,
+// 		'sidebar': Sidebar
+// 	}
+// }
 </script>
 <style>
 .sidebar{
